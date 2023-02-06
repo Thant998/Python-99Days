364aa5130a5b2bb2fd7ad0aa155c0117dc538c04
October Very Own <95152361+Thant998@users.noreply.github.com>
2023-02-06 10:19:15 +0630

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -6,7 +6,7 @@ window = Tk()
 # Then, you will define the size of the window in width(312) and height(324) using the 'geometry' method
 window.geometry("312x324")
 
-# In order to prevent the window from getting resized you will call 'resizable' method on the window
+# In order to prevent the window from getting resized you will call 'resizable' method on the window.
 window.resizable(0, 0)
 
 #Finally, define the title of the window
