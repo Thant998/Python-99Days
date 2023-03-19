a08b16e5974f522a6d9235a3a3f7b6a6acbdf95e
Central Cee <95152361+Thant998@users.noreply.github.com>
2023-03-19 09:42:54 +0630

    Create view.py

---
+++ Django/view.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from .models import Post
+
+def post_list(request):
+    posts = Post.objects.order_by('-pub_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
