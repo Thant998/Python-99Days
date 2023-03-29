08c88b6d92530ed0b52a645da4b5edb3f41e044d
OG Bobby <95152361+Thant998@users.noreply.github.com>
2023-03-29 16:46:40 +0630

    Update view.py

--- Django/view.py
+++ Django/view.py
@@ -5,6 +5,7 @@ def post_list(request):
     posts = Post.objects.order_by('-pub_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
+
 def post_detail(request, pk):
     post = Post.objects.get(pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
